0f8a8ed8b80da6a67266df9b04f4b70bfcee71d0
Rama Aryo Prambudi <111863206+ramaaryoprambudi@users.noreply.github.com>
2025-11-16 13:27:33 +0700

    Modify database.js to add host and port configuration
    Updated database configuration to include host and port settings.

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,9 @@ config();
 const dbConfig = {
   // Untuk Cloud SQL di Cloud Run, uncomment socketPath dan comment host/port
   //socketPath: "/cloudsql/bara-f786e:asia-southeast2:kings-sql",
-
+  
+  host: process.env.DB_HOST, // ⬅ WAJIB di-set, jangan dibiarkan kosong
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER || "avnadmin",
   password: process.env.DB_PASSWORD || "AVNS_srshgCgm00D1lDvmGOb",
   database: process.env.DB_NAME || "defaultdb",
@@ -16,4 +18,5 @@ const dbConfig = {
 };
 
 export default dbConfig;
+
 
